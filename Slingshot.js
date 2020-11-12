cffbbe22b73605757082417ba717ad3ad1b8516e
SuperSak <potturusaketh@gmail.com>
2020-11-11 18:32:51 -0800

    did stuff and added stuff via terminal

---
+++ Slingshot.js
@@ -0,0 +1,27 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+           length:10,
+           stiffness:0.04, 
+           bodyA:bodyA,
+           pointB:pointB
+        }
+        this.pointB=pointB;
+        this.sling=Constraint.create(options)
+        World.add(world, this.sling);
+    }
+    fly() {
+        this.sling.bodyA=null;
+    }
+    display(){
+        if(this.sling.bodyA){
+            var pA=this.sling.bodyA.position
+            var pB=this.pointB;
+            push()
+            strokeWeight(4)
+            line(pA.x,pA.y,pB.x,pB.y)
+            pop()
+        }
+        
+    }
+}
